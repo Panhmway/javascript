ac5386c17eda2b4aa830a93bb0e890f873b3a713
Panhmway <65887500+Panhmway@users.noreply.github.com>
2022-09-20 15:51:28 +0630

    1 file modified

--- index.js
+++ index.js
@@ -12,5 +12,28 @@ appDiv.innerHTML = `<h1>hello</h1>`;
 // const hello = `I'm ${firstName},a ${2022 - bathYear} years old ${job}!`;
 // console.log(hello);
 
-const age = 19;
-const isOldEnough = (age) => 18;
+// const age = 19;
+// const isOldEnough = (age) => 18;
+//const birthYear =1900;
+
+// const massMarks = 95;
+// const heightMarks = 1.88;
+// const massJhon = 485 ;
+// const heightJhon = 1.76;
+
+// const markBMI = massMarks/(heightMarks*heightMarks);
+// const jhonBMI = massJhon/heightJhon**2;
+// console.log (markBMI,jhonBMI);
+
+// // const markHigher = markBMI>jhonBMI;
+// // console.log(markHigher);
+// //question 2
+// if (markBMI>jhonBMI){
+//   console.log(`Mark's BMI is higher than Jhon's`);
+
+// }else
+// {
+//   console.log(`Jhon's BMI is higher than Jhon's`)
+// }
+
+
