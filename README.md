b3229ce62055b9c1f1048c917e479e060c36bf01
Panhmway <65887500+Panhmway@users.noreply.github.com>
2022-09-20 14:19:33 +0630

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-dqzqku
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-dqzqku)
---
+++ index.js
@@ -0,0 +1,16 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>hello</h1>`;
+
+// const firstName = 'hello';
+// const job = 'go developer';
+// const bathYear = 1990;
+
+// const hello = `I'm ${firstName},a ${2022 - bathYear} years old ${job}!`;
+// console.log(hello);
+
+const age = 19;
+const isOldEnough =age => 18;
